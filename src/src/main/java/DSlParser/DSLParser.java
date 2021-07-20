049f87d259ec4d822d71f6b3f9efdd5415d81ba8
nadermedhet148 <nadermedhet142@gmail.com>
2021-07-20 17:21:15 +0200

    [feat] allow spaces

--- src/src/main/java/DSlParser/DSLParser.java
+++ src/src/main/java/DSlParser/DSLParser.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.function.Function;
 
 @Getter
 public class DSLParser {
@@ -14,6 +15,7 @@ public class DSLParser {
 
     private CalculatorDataModel model;
 
+    private Function<String , String> fixSpaces = (String string) -> string.replace(" " , "");
 
     public DSLParser(ArrayList<String> dslRules){
         this.dslRules = dslRules;
@@ -29,18 +31,22 @@ public class DSLParser {
             if(line[0].contains("equation"))
                 parseEquation(line[1]);
             if(line[0].contains("display"))
-                this.model.getCalculations().add(line[1]);
+                this.model.getCalculations().add(this.fixSpaces.apply(line[1]));
         });
         return model;
     }
     public void parseField(String field){
-        this.model.getFields().add(new Field(field.replace(" " , "") , null));
+        this.model.getFields().add(new Field(this.fixSpaces.apply(field), null));
     }
 
     public void parseEquation(String equation){
         String[] nameAndRule =  equation.split("=");
         String[] rules = nameAndRule[1].split(" ");
-        this.model.getEquations().add(new Equation(nameAndRule[0] , rules[0] , rules[2] , rules[1]));
+        this.model.getEquations().add(new Equation(this.fixSpaces.apply(nameAndRule[0]) ,
+                this.fixSpaces.apply(rules[0]),
+                this.fixSpaces.apply(rules[2]),
+                this.fixSpaces.apply(rules[1])
+        ));
     }
 
 }
